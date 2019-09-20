42a8680b6a0a03df34879825dab7fe169d1f54aa
Rafkat <rafgaleev3@gmail.com>
2019-09-20 12:17:58 +0300

    GG

--- payment/payments/urls.py
+++ payment/payments/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('payments/', views.index, name='index'),
     path('customers/', views.customers, name='customers'),
     path('billers/', views.billers, name='billers'),
-    path('newpayment/', views.new_payment, name='new_payment')
+    path('newpayment/', views.new_payment, name='new_payment'),
 ]
--- payment/payments/views.py
+++ payment/payments/views.py
@@ -5,19 +5,28 @@ from django.template import loader
 from .models import Payment, Customer, Biller
 
 def index(request):
-    latest_payments_list = Payment.objects.order_by('-pub_date')[:5]
+    latest_payments_list = Payment.objects.order_by('-pub_date')[:10]
     context = {'latest_payments_list': latest_payments_list}
     return render(request, 'payment/index.html', context)
 
 def customers(request):
-    return HttpResponse("You're looking at customer")
+    latest_customers_list = Customer.objects.order_by('customer_text')
+    context = {'latest_customers_list': latest_customers_list}
+    return render(request, 'payment/customers.html', context)
 
 
 def billers(request):
-    return HttpResponse("You're looking at biller")
+    latest_billers_list = Biller.objects.order_by('biller_text')
+    context = {'latest_billers_list': latest_billers_list}
+    return render(request, 'payment/billers.html', context)
+
 
 def new_payment(request):
-    return HttpResponse("You're looking at new payment")
+    latest_customers_list = Customer.objects.order_by('customer_text')
+    latest_billers_list = Biller.objects.order_by('biller_text')
+    context = {'latest_customers_list': latest_customers_list,
+               'latest_billers_list': latest_billers_list}
+    return render(request, 'payment/newpayment.html', context)
 
 
 
